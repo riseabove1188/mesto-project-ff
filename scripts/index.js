77b5356f05975975a82d4e1ed3f2a20ae75be8a2
Vasiliy Marakin <riseabove1188@yandex.ru>
2024-05-02 23:45:56 +0300

    финиш

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,33 @@
 // @todo: Темплейт карточки
-
+const cardTemplate = document.querySelector("#card-template").content;
 // @todo: DOM узлы
-
+const cardContainer = document.querySelector(".places__list");
+const addButton = document.querySelector(".profile__add-button");
 // @todo: Функция создания карточки
+const addCard = (card, delCard) => {
+    const cardElement = cardTemplate
+        .querySelector(".places__item")
+        .cloneNode(true);
 
+    const img = cardElement.querySelector(".card__image");
+    img.setAttribute("src", card.link);
+    img.setAttribute("alt", card.name);
+
+    const title = cardElement.querySelector(".card__title");
+    title.textContent = card.name;
+
+    cardContainer.append(cardElement);
+
+    const delButton = cardElement.querySelector(".card__delete-button");
+    delButton.addEventListener("click", () => delCard(cardElement));
+};
 // @todo: Функция удаления карточки
-
+const delCard = (el) => {
+    el.remove();
+};
 // @todo: Вывести карточки на страницу
+addButton.addEventListener("click", () => {
+    const card = initialCards.shift();
+    if (!card) return;
+    addCard(card, delCard);
+});
